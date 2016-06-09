dcbd5a8b870ad362dd3d8dc200b5eacf6579b132
acazesadobe <acazes@adobe.com>
2016-06-09 10:34:27 +0100

    HTML5 Single Player

---
+++ Adobe Analytics v15 - Video Milestone/JS/HTML5 Video/js/HTML5SinglePlayerdelegate.js
@@ -0,0 +1,45 @@
+function myHandler(e) {
+	var video = document.getElementsByTagName('video')[0];
+	var mediaName=document.getElementsByTagName('video')[0].getAttribute("name");
+	var mediaLength = video.duration;
+	var mediaPlayerName = "HTML5 Basic Player";
+	//Define video offset
+	if (video.currentTime > 0) {
+		mediaOffset = Math.floor(video.currentTime);
+	} else {
+		mediaOffset = 0;
+	};
+
+	switch(e.type) {
+	case "play":
+		//Call on video start
+		if (mediaOffset ==0) {
+			s.Media.open(mediaName,mediaLength,mediaPlayerName);
+			s.Media.play(mediaName,mediaOffset);
+		} else {
+			s.Media.play(mediaName,mediaOffset);
+		}
+		break;
+	case "seeking":
+		//Call on scrub start
+		s.Media.stop(mediaName,mediaOffset);
+		break;
+	case "seeked":
+		//Call on scrub stop
+		s.Media.play(mediaName,mediaOffset);
+		break;
+	case "pause":
+		//Call on pause
+		s.Media.stop(mediaName,mediaOffset);
+		break;
+	case "ended":
+		//Call on video complete
+		s.Media.stop(mediaName,mediaOffset);
+		s.Media.close(mediaName);
+		mediaOffset = 0;
+		break;
+	default:
+		break;
+	}
+
+};
